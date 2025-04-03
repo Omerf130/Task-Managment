4ff6d4f55f6ab96e7ecaa485404b026b58dc5395
Omerf130 <ofaclieru@gmail.com>
2025-04-03 13:09:34 +0300

    basic react layout created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router";
+import classes from "./index.module.scss";
+import { Nav } from "./components/nav/Nav";
+import {
+  ThemeProvider,
+  createTheme,
+  useColorScheme,
+} from "@mui/material/styles";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = createTheme({
+    colorSchemes: {
+      dark: true,
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={theme}>
+      <div className={classes.app}>
+        <Nav />
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/home/Home.tsx'
+import Login from './pages/login/Login.tsx'
+import Register from './pages/register/Register.tsx'
+import Admin from './pages/admin/Admin.tsx'
+import "./assets/locales/i18n.js";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin" element={<Admin/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
